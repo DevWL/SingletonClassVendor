152a12ee2f5092d84bcc6b860c55e4a4161c5677
GlupiJas <w.liszkiewicz@gmail.com>
2016-09-29 03:48:44 +0200

    new tests and comment update

--- singleton.php
+++ singleton.php
@@ -4,18 +4,18 @@ namespace wl;
 
 /**
  * @dosc allows only one instance for each extending class.
- * it acts a litle bit as registry from the Singleton abstract class point of view
- * but it provides a valid singleton behaviour for its children classes (extended)
+ * it acts a litle bit as registry from the SingletonClassVendor abstract class point of view
+ * but it provides a valid singleton behaviour for its children classes
  * @example use it for database connection, config setup...
  * Be aware, the singleton pattern is consider to be an anti-pattern
  * because it can be hard to debug.
  * In most cases you do not need to use singleton pattern
  * so take a longer moment to think about it before you use it.
  */
-abstract class Singleton
+abstract class SingletonClassVendor
 {
     /**
-     *  holds an single instance of a class
+     *  holds an single instance of the child class
      *
      *  @var array of objects
      */
@@ -63,13 +63,15 @@ abstract class Singleton
  */
 
 /**
- *  @docs Config class by extending Singleton abstract class implements singleton pattern
- *  __constructor must be set to protected for two reasons: 
+ *  @example Database class by extending SingletonClassVendor abstract class becomes fully functional singleton
+ *  __constructor must be set to protected becaouse: 
  *   1 to allow instansiation from parent class 
  *   2 to prevent direct instanciation of object with "new" keword.
+ *   3 to meet requierments of SingletonClassVendor abstract class
  */
-class Database extends Singleton
+class Database extends SingletonClassVendor
 {
+    public $type = "SomeClass";
     protected function __construct(){
         echo "DDDDDDDDD". PHP_EOL; // remove this line after testing
     }
@@ -77,47 +79,44 @@ class Database extends Singleton
 
 
 /**
- *  @docs Config class by extending Singleton abstract class implements singleton pattern
- *  __constructor must be set to protected for two reasons: 
+ *  @example Config class by extending SingletonClassVendor abstract class becomes fully functional singleton
+ *  __constructor must be set to protected becaouse: 
  *   1 to allow instansiation from parent class 
  *   2 to prevent direct instanciation of object with "new" keword.
+ *   3 to meet requierments of SingletonClassVendor abstract class
  */
-class Config extends Singleton
+class Config extends SingletonClassVendor
 {
+    public $name = "Config";
     protected function __construct(){
         echo "CCCCCCCCCC" . PHP_EOL; // remove this line after testing
     }
 }
 
 /**
- *  @example create new Database
+ *  @testing
  */
 $bd1 = Database::getInstance(); // new
 $bd2 = Database::getInstance(); // old
-
-/**
- *  @example create new Config 
- */
 $bd3 = Config::getInstance(); // new
 $bd4 = Config::getInstance(); // old
-
-
-/**
- *  @example more testing
- */
 $bd5 = Config::getInstance(); // old
 $bd6 = Database::getInstance(); // old
-
 $bd7 = Database::getInstance(); // old
 $bd8 = Config::getInstance(); // old
 
+echo PHP_EOL."COMPARE ALL DATABASE INSTANCES".PHP_EOL;
+var_dump($bd1);
+echo '$bd1 === $bd2' . ($bd1 === $bd2)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
+echo '$bd2 === $bd6' . ($bd2 === $bd6)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
+echo '$bd6 === $bd7' . ($bd6 === $bd7)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
 
-echo PHP_EOL. PHP_EOL;
-echo get_class($bd1)  . PHP_EOL;
-echo get_class($bd2)  . PHP_EOL;
-echo get_class($bd3)  . PHP_EOL;
-echo get_class($bd4)  . PHP_EOL;
-echo get_class($bd5)  . PHP_EOL;
-echo get_class($bd6)  . PHP_EOL;
-echo get_class($bd7)  . PHP_EOL;
-echo get_class($bd8)  . PHP_EOL;
+echo PHP_EOL;
+
+echo PHP_EOL."COMPARE ALL CONFIG INSTANCES". PHP_EOL;
+var_dump($bd3);
+echo '$bd3 === $bd4' . ($bd3 === $bd4)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
+echo '$bd4 === $bd5' . ($bd4 === $bd5)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
+echo '$bd5 === $bd8' . ($bd5 === $bd8)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
+
+
