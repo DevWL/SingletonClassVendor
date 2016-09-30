baab891b17614d635fb11dbffcaff8c8bd14e1e0
GlupiJas <w.liszkiewicz@gmail.com>
2016-09-30 03:43:49 +0200

    added final to getInstance method some comment changes

--- SingletonClassVendor.php
+++ SingletonClassVendor.php
@@ -3,12 +3,12 @@ namespace wl;
 
 
 /**
+ * @author DevWL
  * @dosc allows only one instance for each extending class.
  * it acts a litle bit as registry from the SingletonClassVendor abstract class point of view
  * but it provides a valid singleton behaviour for its children classes
- * @example use it for database connection, config setup...
  * Be aware, the singleton pattern is consider to be an anti-pattern
- * because it can be hard to debug.
+ * mostly because it can be hard to debug and it comes with some limitations.
  * In most cases you do not need to use singleton pattern
  * so take a longer moment to think about it before you use it.
  */
@@ -25,7 +25,7 @@ abstract class SingletonClassVendor
      *  @desc provides a single slot to hold an instance interchanble between all child classes.
      *  @return object
      */
-    public static function getInstance(){
+    public static final function getInstance(){
         $class = get_called_class(); // or get_class(new static());
         if(!isset(self::$instance[$class]) || !self::$instance[$class] instanceof $class){
             self::$instance[$class] = new static(); // create and instance of child class which extends Singleton super class
@@ -59,11 +59,11 @@ abstract class SingletonClassVendor
 }
 
 /**
- * ---------------------------------------------- USE EXAMPLE BELLOW---------------------------------------------------
+ * EXAMPLE
  */
 
 /**
- *  @example Database class by extending SingletonClassVendor abstract class becomes fully functional singleton
+ *  @example 1 - Database class by extending SingletonClassVendor abstract class becomes fully functional singleton
  *  __constructor must be set to protected becaouse: 
  *   1 to allow instansiation from parent class 
  *   2 to prevent direct instanciation of object with "new" keword.
@@ -79,11 +79,7 @@ class Database extends SingletonClassVendor
 
 
 /**
- *  @example Config class by extending SingletonClassVendor abstract class becomes fully functional singleton
- *  __constructor must be set to protected becaouse: 
- *   1 to allow instansiation from parent class 
- *   2 to prevent direct instanciation of object with "new" keword.
- *   3 to meet requierments of SingletonClassVendor abstract class
+ *  @example 2 - Config ...
  */
 class Config extends SingletonClassVendor
 {
@@ -94,7 +90,7 @@ class Config extends SingletonClassVendor
 }
 
 /**
- *  @testing
+ *  TESTING
  */
 $bd1 = Database::getInstance(); // new
 $bd2 = Database::getInstance(); // old
@@ -107,16 +103,16 @@ $bd8 = Config::getInstance(); // old
 
 echo PHP_EOL."COMPARE ALL DATABASE INSTANCES".PHP_EOL;
 var_dump($bd1);
-echo '$bd1 === $bd2' . ($bd1 === $bd2)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
-echo '$bd2 === $bd6' . ($bd2 === $bd6)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
-echo '$bd6 === $bd7' . ($bd6 === $bd7)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
+echo '$bd1 === $bd2' . ($bd1 === $bd2)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL; // TRUE
+echo '$bd2 === $bd6' . ($bd2 === $bd6)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL; // TRUE
+echo '$bd6 === $bd7' . ($bd6 === $bd7)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL; // TRUE
 
 echo PHP_EOL;
 
 echo PHP_EOL."COMPARE ALL CONFIG INSTANCES". PHP_EOL;
 var_dump($bd3);
-echo '$bd3 === $bd4' . ($bd3 === $bd4)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
-echo '$bd4 === $bd5' . ($bd4 === $bd5)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
-echo '$bd5 === $bd8' . ($bd5 === $bd8)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL;
+echo '$bd3 === $bd4' . ($bd3 === $bd4)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL; // TRUE
+echo '$bd4 === $bd5' . ($bd4 === $bd5)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL; // TRUE
+echo '$bd5 === $bd8' . ($bd5 === $bd8)? ' TRUE' . PHP_EOL: ' FALSE' . PHP_EOL; // TRUE
 
 
